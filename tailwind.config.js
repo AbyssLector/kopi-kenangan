6cc5c1827e076b9c87a5ef5bd43f2b44de1ff1cd
Naufal Ulhaq <naufalulhaq.13@gmail.com>
2022-12-02 18:34:45 +0700

    first push, home and login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'home.html', 'login.html'],
+  theme: {
+    extend: {
+      colors: {
+        kopkengrey: '#F4F4F4',
+        kopkenred: '#BB2028'
+      }
+    },
+  },
+  plugins: [],
+}
